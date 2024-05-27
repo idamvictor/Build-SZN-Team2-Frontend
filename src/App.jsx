8b2fe768b81ef3dd17963e01fec7cd2ad13b61bf
idamvictor <idamvictorx1@gmail.com>
2024-05-27 07:01:49 +0100

    bgufix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import './App.css'
 // import Signin from './pages/Signin'
 // import { Signup } from "./pages/Signup"
-import { EmailVerification } from "./pages/EmailVerification"
+// import { EmailVerification } from "./pages/EmailVerification"
 import { Homepage } from "./pages/Homepage"
 // import { Onboarding } from "./pages/Onboarding"
 // import { ForgotPassword } from "./pages/ForgotPassword"
@@ -49,7 +49,7 @@ function App() {
           <Route path="Signin" element={<Signin />} />
           <Route path="SubscriptionPlan" element={<SubscriptionPlan />} />
           <Route path="Onboarding" element={<Onboarding />} />
-          <Route path="EmailVerification" element={<EmailVerification />} />
+          {/* <Route path="EmailVerification" element={<EmailVerification />} /> */}
           <Route path="Homepage" element={<Homepage />} />
           <Route path="Onboarding" element={<Onboarding />} />
           {/* <Route path="ForgotPassword" element={<ForgotPassword />} /> */}
